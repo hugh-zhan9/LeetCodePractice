9c4c1b7854becf82b6b9374b35f128a4db6fb3ee
zhangyk <yukun.zhang@higentec.com>
2021-06-11 17:05:15 +0800

    冒泡排序、选择排序实现

--- src/main/java/arithmetic/sort/Sort.java
+++ src/main/java/arithmetic/sort/Sort.java
@@ -6,7 +6,38 @@ package arithmetic.sort;
  * @since 2021/6/11 16:18
  */
 public class Sort {
-    // 插入排序，a表示数组，n表示数组大小
+
+    /**
+     * 冒泡排序，a表示数组，n表示数组大小
+     * @param a
+     * @param n
+     */
+    public void bubbleSort(int[] a, int n) {
+        if (n <= 1) return;
+
+        for (int i = 0; i < n; ++i) {
+            // 提前退出冒泡循环的标志位
+            boolean flag = false;
+            for (int j=0; j < n-i- 1; ++j) {
+                // 交换
+                if (a[j] > a[j+1]) {
+                    int tmp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = tmp;
+                    // 表示有数据交换
+                    flag = true;
+                }
+            }
+            // 没有数据交换，提前退出
+            if (!flag) break;
+        }
+    }
+
+    /**
+     * 插入排序，a表示数组，n表示数组大小
+     * @param a
+     * @param n
+     */
     public void insertionSort(int[] a, int n) {
         if (n <= 1) return;
 
@@ -16,12 +47,36 @@ public class Sort {
             // 查找插入的位置
             for (; j >= 0; --j) {
                 if (a[j] > value) {
-                    a[j+1] = a[j];  // 数据移动
+                    // 数据移动
+                    a[j+1] = a[j];
                 } else {
                     break;
                 }
             }
-            a[j+1] = value; // 插入数据
+            // 插入数据
+            a[j+1] = value;
+        }
+    }
+
+    /**
+     * 选择排序
+     * @param a
+     * @param n
+     */
+    public void selectionSort(int[] a, int n) {
+        if (n <= 1) return;
+        for(int i=0; i<n-1; i++){
+            int j = i+1;
+            int min = i;
+            // 从未排序区选择最小的
+            for(; j<n; j++){
+                if(a[j] < a[min]){
+                    min = j;
+                }
+            }
+            int temp = a[i];
+            a[i] = a[min];
+            a[min] = temp;
         }
     }
 
