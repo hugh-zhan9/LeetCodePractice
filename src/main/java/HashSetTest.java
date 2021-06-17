322db1edbdfd95a6a67dd045592f5b7023d07ee6
zhangyk <yukun.zhang@higentec.com>
2021-06-17 11:06:10 +0800

    新增题解

--- src/main/java/HashSetTest.java
+++ src/main/java/HashSetTest.java
@@ -1,7 +1,9 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Twitter上的一道题目
@@ -21,5 +23,68 @@ public class HashSetTest {
         System.out.println(set.contains(sb));   // should print true
         sb.append("oops");
         System.out.println(set.contains(sb));   // should print false
+    }
+}
+
+
+/**
+ * Twitter上的一种解法
+ */
+
+class StringBuilderInHashMap{
+    public static void main(String[] args) throws Exception {
+        List<StringBuilder> list = Collections.synchronizedList(new ArrayList<>(Arrays.asList(new StringBuilder(""))));
+        StringBuilder sb = IntStream.of(1).mapToObj( n ->{
+            int targetKeyHash;
+            targetKeyHash = (targetKeyHash = list.get(0).hashCode()) ^ (targetKeyHash >>> 16);
+
+            int targetTab = 63 & targetKeyHash;
+            while (list.size() < 40){
+                StringBuilder nsb = new StringBuilder();
+                int keyHash;
+                keyHash = (keyHash = nsb.hashCode()) ^ (keyHash >>> 16);
+                int tab = 63 & keyHash;
+
+                if (tab == targetTab){
+                    list.add(nsb);
+                }
+            }
+
+            ExecutorService a = Executors.newFixedThreadPool(8);
+            AtomicInteger b = new AtomicInteger(0);
+            int c = list.stream().mapToInt(Object::hashCode).min().getAsInt();
+            for (int i=0; i<8; i++){
+                a.execute(()->{
+                    while (b.get() < 1){
+                        StringBuilder nsb = new StringBuilder("");
+
+                        if (nsb.hashCode() == c){
+                            if (b.get() < 1){
+                                list.add(nsb);
+                                b.getAndIncrement();
+                            }
+                            break;
+                        }
+                    }
+                });
+            }
+
+            while (b.get() < 1){
+                StringBuilder nsb = new StringBuilder("");
+                if (nsb.hashCode() == c){
+                    list.add(nsb);
+                    b.getAndIncrement();
+                    break;
+                }
+            }
+            a.shutdownNow();
+            return list.get(list.size() -1);
+        }).findFirst().orElseThrow(Exception::new);
+
+        Set<StringBuilder> set = new HashSet<>(list);
+        set.add(sb);
+        System.out.println(set.contains(sb));
+        sb.append("oops");
+        System.out.println(set.contains(sb));
     }
 }
