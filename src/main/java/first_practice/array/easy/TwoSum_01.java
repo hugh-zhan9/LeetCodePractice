fc4888a992023ecf18743aac9316ae6d44b7d9f0
hugh-zhan9 <49809663+hugh-zhan9@users.noreply.github.com>
2021-08-05 10:51:18 +0800

    2021-08-05 LeetCode TwoSum

--- src/main/java/first_practice/array/easy/TwoSum_01.java
+++ src/main/java/first_practice/array/easy/TwoSum_01.java
@@ -39,44 +39,71 @@ package first_practice.array.easy;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author zyk
  * @description
  * @since 2021/8/5 6:48
  */
+
 public class TwoSum_01 {
 
     /**
-     * 暴力解法，时间复杂度为 O(n²)
+     * 暴力解法，时间复杂度为 O(n²)，空间复杂度O(1)
      * @param nums
      * @param target
      * @return
      */
     public int[] twoSum(int[] nums, int target) {
-        int[] result = null;
-        for(int i=0; i<nums.length ;i++){
-            for (int j=nums.length-1; j>i;j--){
-                if (target == (nums[i]+nums[j])){
-                    result = new int[]{i, j};
+        int[] t = null;
+        for (int a = 0; a<nums.length; a++){
+            for(int i = a+1;i<nums.length; i++){
+                if (nums[a]+nums[i]==target){
+                    t = new int[]{a,i};
                     break;
                 }
             }
         }
-        return result;
+        return t;
     }
 
     /**
-     * 哈希表解法，时间复杂度为 O(n)
+     * 执行用时：40 ms, 在所有 Java 提交中击败了 35.97% 的用户
+     * 内存消耗：38.6 MB, 在所有 Java 提交中击败了 54.20% 的用户
+     */
+
+
+    /**
+     * 哈希表解法，时间复杂度为 O(n)，空间复杂度O(n)
      * @param nums
      * @param target
      * @return
      */
     public int[] twoSum1(int[] nums, int target){
         int[] result = null;
+        Map<Integer,Integer> targetMap = new HashMap();
+        for (int i=0; i<nums.length; i++){
+            if (targetMap.containsKey(nums[i])){
+                result = new int[]{targetMap.get(nums[i]), i};
+            }else {
+                targetMap.put(target-nums[i],i);
+            }
+        }
         return result;
     }
 
-    public static void main(String[] args) {
+    /**
+     * 执行用时：4 ms, 在所有 Java 提交中击败了 47.86% 的用户
+     * 内存消耗：39.4 MB, 在所有 Java 提交中击败了 5.54% 的用户
+     */
 
+    public static void main(String[] args) {
+        TwoSum_01 solution = new TwoSum_01();
+        int[] test = new int[] {2,7,11,15};
+        int target = 9;
+        int[] ints = solution.twoSum1(test, target);
+        System.out.println(""+ints[0] + ints[1]);
     }
 }
