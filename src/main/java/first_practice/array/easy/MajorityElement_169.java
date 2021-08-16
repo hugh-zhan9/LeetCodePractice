ca646635e036e9aed7caeb019a13e5df0a6749c8
晚青 <49809663+hugh-zhan9@users.noreply.github.com>
2021-08-16 11:24:51 +0800

    2021-08-16 LeetCode 169_MajorityElement

--- src/main/java/first_practice/array/easy/MajorityElement_169.java
+++ src/main/java/first_practice/array/easy/MajorityElement_169.java
@@ -22,9 +22,7 @@ package first_practice.array.easy;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
-import algorithm_implementation.Sort;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -66,12 +64,19 @@ public class MajorityElement_169 {
     /** 使用计数排序实现一下，发现如果有负数的话使用之前的实现不方便，而且空间复杂度也不够 */
     /**
      * 找到出现最多的元素，出现次数大于 n/2 的元素，---> 排序之后选择 第n/2个元素。
-     * 时间复杂度O(n)的排序算法就是桶排序
+     * 时间复杂度O(n)的排序算法就是桶排序。但是桶排序的可见复杂度也不够
      */
     public int majorityElement2(int[] nums) {
-        Arrays.sort(nums);
+        /* 看题解直接使用了API...
+        Arrays.sort(nums); */
         return nums[nums.length/2];
     }
+
+    /**
+     * 执行用时：2 ms, 在所有 Java 提交中击败了63.03%的用户
+     * 内存消耗：44.2 MB, 在所有 Java 提交中击败了56.32%的用户
+     */
+
 
     /**
      * 摩尔投票法：时间复杂度O(N) 空间复杂度O(1)
@@ -105,6 +110,22 @@ public class MajorityElement_169 {
 
 
     /** 分治算法 */
+    /**
+     * 如果数 a 是数组 nums 的众数，将 nums 分成两部分，那么 a 必定是至少一部分的众数。
+     * 可以使用反证法来证明这个结论。
+     * 假设 a 既不是左半部分的众数，也不是右半部分的众数，那么 a 出现的次数少于 l/2+r/2 次，其中l和r分别是左半部分和右半部分的长度。
+     * 由于 l/2+r/2 <= (l+r)/2，说明 a 也不是数组 nums 的众数，因此出现了矛盾。所以这个结论是正确的。
+     *
+     * 这样以来，就可以使用分治法解决这个问题：
+     * 将数组分成左右两部分，分别求出左半部分的众数 a1 以及右半部分的众数 a2，随后在 a1 和 a2 中选出正确的众数。
+     */
+    /**
+     * 使用经典的分治算法递归求解，直到所有的子问题都是长度为 1 的数组。
+     * 长度为 1 的子数组中唯一的数显然是众数，直接返回即可。
+     * 如果回溯后某区间的长度大于 1，必须将左右子区间的值合并。
+     * 如果它们的众数相同，那么显然这一段区间的众数是它们相同的值。
+     * 否则，需要比较两个众数在整个区间内出现的次数来决定该区间的众数。
+     */
     public int majorityElement4(int[] nums){
         return majorityElementRec(nums, 0, nums.length - 1);
     }
