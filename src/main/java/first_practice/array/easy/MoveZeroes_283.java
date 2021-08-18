8ca95e9bfb937c904f8d56069bc32de28230fcc6
hugh-zhan9 <hugh_zhan9@163.com>
2021-08-19 07:04:43 +0800

    2021-08-119 LeetCode 283_MoveZeroes

---
+++ src/main/java/first_practice/array/easy/MoveZeroes_283.java
@@ -0,0 +1,62 @@
+package first_practice.array.easy;
+
+/**
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ *
+ * 示例:
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ *
+ * 说明:
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ *
+ * 通过次数445,501提交次数697,251
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/move-zeroes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * @author zyk
+ * @description
+ * @since 2021/8/19 6:38
+ */
+public class MoveZeroes_283 {
+
+    /** 双指针 */
+    public void moveZeroes(int[] nums) {
+        int i=0;
+        for(int j=0; j<nums.length; j++){
+            if (nums[i]!=0){
+                i++;
+                continue;
+            }
+            if (i==nums.length){
+                return;
+            }
+            while (nums[i]==0 && nums[j]!=0){
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                i++;
+            }
+        }
+    }
+
+    /**
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：38.5 MB, 在所有 Java 提交中击败了70.55%的用户
+     */
+
+    public static void main(String[] args) {
+        MoveZeroes_283 solution = new MoveZeroes_283();
+        int[] nums = new int[]{0,1,0,3,12};
+        solution.moveZeroes(nums);
+        for (int i : nums) {
+            System.out.println(i);
+        }
+
+    }
+}
