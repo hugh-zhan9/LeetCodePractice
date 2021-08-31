3be181a23b8a4609d35e346daa6658bc91e60030
晚青 <49809663+hugh-zhan9@users.noreply.github.com>
2021-08-31 18:18:41 +0800

    2021-08-31 剑指Offer 18_DeleteNode

---
+++ DeleteNode_18.java
@@ -0,0 +1,61 @@
+package first_practice.list.easy.剑指Offer;
+
+/**
+ * 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+ * 返回删除后的链表的头节点。
+ * 注意：此题对比原题有改动
+ *
+ * 示例 1:
+ * 输入: head = [4,5,1,9], val = 5
+ * 输出: [4,1,9]
+ * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ *
+ * 示例 2:
+ * 输入: head = [4,5,1,9], val = 1
+ * 输出: [4,5,9]
+ * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ *  
+ *
+ * 说明：
+ * 题目保证链表中节点的值互不相同
+ * 若使用 C 或 C++ 语言，你不需要 free 或 delete 被删除的节点
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * @author zyk
+ * @description
+ * @since 2021/8/31 11:24
+ */
+public class DeleteNode_18 {
+
+    public ListNode deleteNode(ListNode head, int val) {
+        if (head == null){
+            return head;
+        }
+        ListNode header = new ListNode(-1);
+        header.next = head;
+        ListNode prev = header;
+        ListNode post = head;
+        while (post!= null){
+            ListNode next = post.next;
+            if (post.val == val){
+                post = next;
+                prev.next = next;
+            }else {
+                prev = post;
+                post = next;
+            }
+        }
+        return header.next;
+    }
+
+    /**
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：37.3 MB, 在所有 Java 提交中击败了99.10%的用户
+     */
+
+}
