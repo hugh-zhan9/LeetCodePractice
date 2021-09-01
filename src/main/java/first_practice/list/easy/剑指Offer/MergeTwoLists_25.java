9807339a56cc0a27eb20495ae7a72a82c81be7e5
晚青 <49809663+hugh-zhan9@users.noreply.github.com>
2021-09-01 18:01:23 +0800

    2021-09-01 剑指Offer 25_MergeTwoList

---
+++ src/main/java/first_practice/list/easy/剑指Offer/MergeTwoLists_25.java
@@ -0,0 +1,29 @@
+package first_practice.list.easy.剑指Offer;
+
+/**
+ * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+ *
+ * 示例1：
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
+ * 限制：
+ * 0 <= 链表长度 <= 1000
+ *
+ * 注意：本题与主站 21 题相同：https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * @author zyk
+ * @description
+ * @since 2021/9/1 11:05
+ */
+public class MergeTwoLists_25 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        return new ListNode(1);
+    }
+}
