78cb5fbd289269e07b215c7405192cae0498d04d
晚青 <49809663+hugh-zhan9@users.noreply.github.com>
2021-09-01 17:59:46 +0800

    2021-09-01 剑指Offer 24_ReverseList

---
+++ src/main/java/first_practice/list/easy/剑指Offer/reverseList_24.java
@@ -0,0 +1,61 @@
+package first_practice.list.easy.剑指Offer;
+
+/**
+ * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+ *
+ *
+ * 示例:
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ *  
+ *
+ * 限制：
+ * 0 <= 节点个数 <= 5000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * @author zyk
+ * @description
+ * @since 2021/9/1 10:01
+ */
+public class reverseList_24 {
+
+    /** 迭代 */
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    /**
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：38.3 MB, 在所有 Java 提交中击败了20.59%的用户
+     */
+
+    /** 递归 */
+    public ListNode reverseList2(ListNode head){
+        // 终止条件
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    /**
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：38.5 MB, 在所有 Java 提交中击败了6.19%的用户
+     */
+}
