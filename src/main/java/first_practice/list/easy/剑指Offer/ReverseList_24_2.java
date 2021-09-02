26a3cacedb4cb92716bb797d1b6517a198e127af
hugh-zhan9 <hugh_zhan9@163.com>
2021-09-02 20:04:57 +0800

    2021-09-02 剑指Offer II 24_ReverseList

---
+++ src/main/java/first_practice/list/easy/剑指Offer/ReverseList_24_2.java
@@ -0,0 +1,64 @@
+package first_practice.list.easy.剑指Offer;
+
+/**
+ * 给定单链表的头节点 head ，请反转链表，并返回反转后的链表的头节点。
+ *
+ * 示例 1：
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ *
+ * 示例 2：
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ *
+ * 示例 3：
+ * 输入：head = []
+ * 输出：[]
+ *  
+ * 提示：
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 <= Node.val <= 5000
+ *  
+ *
+ * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+ *
+ * 注意：本题与主站 206 题相同： https://leetcode-cn.com/problems/reverse-linked-list/
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/UHnkqh
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * @author zyk
+ * @description
+ * @since 2021/9/2 19:46
+ */
+public class ReverseList_24_2 {
+
+    /** 递归 */
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode listNode = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return listNode;
+    }
+
+
+    /** 迭代 */
+    public ListNode reverseList2(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+}
