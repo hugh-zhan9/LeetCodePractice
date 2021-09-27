5de16549fd942efe4bc0c694bebb90014dff92ee
晚青 <49809663+hugh-zhan9@users.noreply.github.com>
2021-09-27 11:15:28 +0800

    2021-09-27 LeetCode 168_ConvertToTitle

--- src/main/java/first_practice/string/easy/ConvertToTitle_168.java
+++ src/main/java/first_practice/string/easy/ConvertToTitle_168.java
@@ -47,23 +47,76 @@ package first_practice.string.easy;
  */
 public class ConvertToTitle_168 {
 
+    /** 思路差不太多，还是没有想到要把它减去1，想模拟辗转相除法 */
     public String convertToTitle(int columnNumber) {
         StringBuffer r = new StringBuffer();
-        int count;
+        int count = 0,val;
         while (columnNumber > 0){
-            count = columnNumber%26;
+            val = columnNumber%26;
             columnNumber = columnNumber/26;
-            char i = (char) (count +'A' -1);
-            r.append(i);
+            if (val>0){
+                char i = columnNumber==0 && val==1 ? (char) (val*26 + 'A' -1) : (char) (val + 'A' -1);
+                r.append(i);
+            }
+            count++;
+
         }
         r.reverse();
         return r.toString();
     }
 
 
-    public static void main(String[] args) {
-        ConvertToTitle_168 convert = new ConvertToTitle_168();
-        System.out.println(convert.convertToTitle(28));
+    /** 提交的写法，过不了26的倍数 */
+    public String convertToTitle2(int columnNumber) {
+        StringBuffer r = new StringBuffer();
+        while (columnNumber>0){
+            int val = columnNumber/26;
+            if (val>26){
+                int s = columnNumber%26;
+                char i = (char) (s + 'A' -1);
+                r.append(i);
+                columnNumber = columnNumber/26;
+                continue;
+            }else {
+                int s = columnNumber%26;
+                char i = (char) (s + 'A' -1);
+                r.append(i);
+            }
+            if (val != 0){
+                char i = (char) (val + 'A' -1);
+                r.append(i);
+            }
+            break;
+        }
+        r.reverse();
+        return r.toString();
     }
+
+
+
+    /**
+     * 题解解释得复杂了，和正常 0~25 的 26 进制相比，本质上就是每一位多加了 1。
+     * 假设 A == 0，B == 1，那么 AB = 26 * 0 + 1 * 1，而现在 AB = 26 * (0 + 1) + 1 * (1 + 1)。
+     * 所以只要在处理每一位的时候减 1，就可以按照正常的 26 进制来处理
+     */
+
+
+    public String convertToTitle3(int columnNumber) {
+        StringBuffer sb = new StringBuffer();
+        while (columnNumber > 0) {
+            // 这两行解决了余数是0的时候导致的输出@
+            int a0 = (columnNumber - 1) % 26 + 1;
+            sb.append((char)(a0 - 1 + 'A'));
+            // 如果去掉 -a0 701过不了
+            columnNumber = (columnNumber - a0) / 26;
+        }
+        return sb.reverse().toString();
+    }
+
+
+    /**
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：35.4 MB, 在所有 Java 提交中击败了69.06%的用户
+     */
 
 }
