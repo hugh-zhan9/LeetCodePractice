134fe3d869e905c502fb0eaca781df370fc1335f
hugh-zhan9 <hugh_zhan9@163.com>
2021-09-30 06:21:29 +0800

    2021-09-30 LeetCode 257_BinaryTreePaths

--- src/main/java/first_practice/string/easy/BinaryTreePaths_257.java
+++ src/main/java/first_practice/string/easy/BinaryTreePaths_257.java
@@ -24,7 +24,10 @@ package first_practice.string.easy;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @author zyk
@@ -33,9 +36,71 @@ import java.util.List;
  */
 public class BinaryTreePaths_257 {
 
+    /** 深度优先算法 */
     public List<String> binaryTreePaths(TreeNode root) {
-        return null;
+        List<String> paths = new ArrayList<String>();
+        constructPaths(root, "", paths);
+        return paths;
     }
+
+    public void constructPaths(TreeNode root, String path, List<String> paths) {
+        if (root != null) {
+            StringBuffer pathSB = new StringBuffer(path);
+            pathSB.append(Integer.toString(root.val));
+            if (root.left == null && root.right == null) {  // 当前节点是叶子节点
+                paths.add(pathSB.toString());  // 把路径加入到答案中
+            } else {
+                pathSB.append("->");  // 当前节点不是叶子节点，继续递归遍历
+                constructPaths(root.left, pathSB.toString(), paths);
+                constructPaths(root.right, pathSB.toString(), paths);
+            }
+        }
+    }
+
+    /**
+     * 执行用时：1 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：38.6 MB, 在所有 Java 提交中击败了47.17%的用户
+     */
+
+
+    /** 广度优先算法 */
+    public List<String> binaryTreePaths2(TreeNode root) {
+        List<String> paths = new ArrayList<String>();
+        if (root == null) {
+            return paths;
+        }
+        Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        Queue<String> pathQueue = new LinkedList<String>();
+
+        nodeQueue.offer(root);
+        pathQueue.offer(Integer.toString(root.val));
+
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.poll();
+            String path = pathQueue.poll();
+
+            if (node.left == null && node.right == null) {
+                paths.add(path);
+            } else {
+                if (node.left != null) {
+                    nodeQueue.offer(node.left);
+                    pathQueue.offer(new StringBuffer(path).append("->").append(node.left.val).toString());
+                }
+
+                if (node.right != null) {
+                    nodeQueue.offer(node.right);
+                    pathQueue.offer(new StringBuffer(path).append("->").append(node.right.val).toString());
+                }
+            }
+        }
+        return paths;
+    }
+
+    /**
+     * 执行用时：2 ms, 在所有 Java 提交中击败了64.53%的用户
+     * 内存消耗：38.4 MB, 在所有 Java 提交中击败了83.67%的用户
+     */
+
 
 }
 
