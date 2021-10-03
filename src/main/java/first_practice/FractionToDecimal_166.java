f3105ff45f183ee7e4b6686c0fcff9804a90b5bd
hugh-zhan9 <hugh_zhan9@163.com>
2021-10-03 22:48:20 +0800

    2021-10-03 LeetCode 166_FractionToDecimal

---
+++ src/main/java/first_practice/FractionToDecimal_166.java
@@ -0,0 +1,121 @@
+package first_practice;
+
+/**
+ * 给定两个整数，分别表示分数的分子 numerator 和分母 denominator，以字符串形式返回小数 。
+ * 如果小数部分为循环小数，则将循环的部分括在括号内。
+ * 如果存在多个答案，只需返回任意一个 。
+ * 对于所有给定的输入，保证答案字符串的长度小于 104 。
+ *
+ *  
+ *
+ * 示例 1：
+ * 输入：numerator = 1, denominator = 2
+ * 输出："0.5"
+ *
+ * 示例 2：
+ * 输入：numerator = 2, denominator = 1
+ * 输出："2"
+ *
+ * 示例 3：
+ * 输入：numerator = 2, denominator = 3
+ * 输出："0.(6)"
+ *
+ * 示例 4：
+ * 输入：numerator = 4, denominator = 333
+ * 输出："0.(012)"
+ *
+ * 示例 5：
+ * 输入：numerator = 1, denominator = 5
+ * 输出："0.2"
+ *  
+ *
+ * 提示：
+ * -231 <= numerator, denominator <= 231 - 1
+ * denominator != 0
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fraction-to-recurring-decimal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zyk
+ * @description
+ * @since 2021/10/3 20:45
+ */
+public class FractionToDecimal_166 {
+
+    public String fractionToDecimal(int numerator, int denominator) {
+        StringBuilder sb = new StringBuilder();
+        long a = numerator, b = denominator;
+        if (a > 0 && b < 0 || a < 0 && b > 0) sb.append('-');
+        a = Math.abs(a);
+        b = Math.abs(b);
+        sb.append(a / b);
+        if (a % b == 0) return sb.toString();
+        sb.append('.');
+        Map<Long, Integer> map = new HashMap<>();
+        while ((a = (a % b) * 10) > 0 && !map.containsKey(a)) {
+            map.put(a, sb.length());
+            sb.append(a / b);
+        }
+        if (a == 0) return sb.toString();
+        return sb.insert(map.get(a).intValue(), '(').append(')').toString();
+    }
+
+    /**
+     * 执行用时：1 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：35.6 MB, 在所有 Java 提交中击败了89.69%的用户
+     */
+
+    public String fractionToDecimal2(int numerator, int denominator) {
+        long numeratorLong = (long) numerator;
+        long denominatorLong = (long) denominator;
+        if (numeratorLong % denominatorLong == 0) {
+            return String.valueOf(numeratorLong / denominatorLong);
+        }
+
+        StringBuffer sb = new StringBuffer();
+        if (numeratorLong < 0 ^ denominatorLong < 0) {
+            sb.append('-');
+        }
+
+        // 整数部分
+        numeratorLong = Math.abs(numeratorLong);
+        denominatorLong = Math.abs(denominatorLong);
+        long integerPart = numeratorLong / denominatorLong;
+        sb.append(integerPart);
+        sb.append('.');
+
+        // 小数部分
+        StringBuffer fractionPart = new StringBuffer();
+        Map<Long, Integer> remainderIndexMap = new HashMap<Long, Integer>();
+        long remainder = numeratorLong % denominatorLong;
+        int index = 0;
+        while (remainder != 0 && !remainderIndexMap.containsKey(remainder)) {
+            remainderIndexMap.put(remainder, index);
+            remainder *= 10;
+            fractionPart.append(remainder / denominatorLong);
+            remainder %= denominatorLong;
+            index++;
+        }
+        if (remainder != 0) { // 有循环节
+            int insertIndex = remainderIndexMap.get(remainder);
+            fractionPart.insert(insertIndex, '(');
+            fractionPart.append(')');
+        }
+        sb.append(fractionPart.toString());
+
+        return sb.toString();
+    }
+
+    /**
+     * 执行用时：1 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：35.8 MB, 在所有 Java 提交中击败了68.53%的用户
+     */
+
+
+}
