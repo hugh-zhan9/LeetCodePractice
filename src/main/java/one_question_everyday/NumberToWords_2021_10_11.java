618526b63590f65f01767f2056d6e77dcb4f87f5
晚青 <49809663+hugh-zhan9@users.noreply.github.com>
2021-10-11 11:00:59 +0800

    2021-10-11 one_question_everyday NumberToWords

---
+++ src/main/java/one_question_everyday/NumberToWords_2021_10_11.java
@@ -0,0 +1,88 @@
+package one_question_everyday;
+
+/**
+ * 将非负整数 num 转换为其对应的英文表示。
+ *
+ *  
+ *
+ * 示例 1：
+ * 输入：num = 123
+ * 输出："One Hundred Twenty Three"
+ *
+ * 示例 2：
+ * 输入：num = 12345
+ * 输出："Twelve Thousand Three Hundred Forty Five"
+ *
+ * 示例 3：
+ * 输入：num = 1234567
+ * 输出："One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+ *
+ * 示例 4：
+ * 输入：num = 1234567891
+ * 输出："One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One"
+ *  
+ *
+ * 提示：
+ * 0 <= num <= 2^31 - 1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/integer-to-english-words
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * @author zyk
+ * @description
+ * @since 2021/10/11 8:46
+ */
+public class NumberToWords_2021_10_11 {
+
+    String[] singles = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    String[] teens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    String[] tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    String[] thousands = {"", "Thousand", "Million", "Billion"};
+
+
+    public String numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i = 3, unit = 1000000000; i >= 0; i--, unit /= 1000) {
+            int curNum = num / unit;
+            if (curNum != 0) {
+                num -= curNum * unit;
+                StringBuffer curr = new StringBuffer();
+                recursion(curr, curNum);
+                curr.append(thousands[i]).append(" ");
+                sb.append(curr);
+            }
+        }
+        return sb.toString().trim();
+    }
+
+    public void recursion(StringBuffer curr, int num) {
+        if (num == 0) {
+            return;
+        } else if (num < 10) {
+            curr.append(singles[num]).append(" ");
+        } else if (num < 20) {
+            curr.append(teens[num - 10]).append(" ");
+        } else if (num < 100) {
+            curr.append(tens[num / 10]).append(" ");
+            recursion(curr, num % 10);
+        } else {
+            curr.append(singles[num / 100]).append(" Hundred ");
+            recursion(curr, num % 100);
+        }
+    }
+
+    /**
+     * 执行用时：3 ms, 在所有 Java 提交中击败了79.30%的用户
+     * 内存消耗：37.7 MB, 在所有 Java 提交中击败了64.18%的用户
+     */
+
+
+    /** 这题确实做的恶心人，但是还是凸显除了我的问题，值得再重新写一次 */
+
+}
