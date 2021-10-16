e13f5a7afdc6009195e59dcad56d65e6b5e9bf50
hugh-zhan9 <hugh_zhan9@163.com>
2021-10-16 10:05:26 +0800

    2021-10-16 LeetCode 448_FindDisappearedNumbers

---
+++ src/main/java/first_practice/hash/easy/FindDisappearedNumbers_448.java
@@ -0,0 +1,58 @@
+package first_practice.hash.easy;
+
+/**
+ * 给你一个含 n 个整数的数组 nums ，其中 nums[i] 在区间 [1, n] 内。
+ * 请你找出所有在 [1, n] 范围内但没有出现在 nums 中的数字，并以数组的形式返回结果。
+ *
+ * 示例 1：
+ * 输入：nums = [4,3,2,7,8,2,3,1]
+ * 输出：[5,6]
+ *
+ * 示例 2：
+ * 输入：nums = [1,1]
+ * 输出：[2]
+ *  
+ *
+ * 提示：
+ * n == nums.length
+ * 1 <= n <= 105
+ * 1 <= nums[i] <= n
+ * 进阶：你能在不使用额外空间且时间复杂度为 O(n) 的情况下解决这个问题吗? 你可以假定返回的数组不算在额外空间内。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author zyk
+ * @description
+ * @since 2021/10/16 9:53
+ */
+public class FindDisappearedNumbers_448 {
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int length = nums.length;
+        List<Integer> list = new ArrayList<>();
+        Map<Integer,Integer> map = new HashMap<>();
+        for (Integer i : nums){
+            map.put(i, map.getOrDefault(i,0)+1);
+        }
+        for (int i=1; i<length+1; i++){
+            if (!map.containsKey(i)){
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 执行用时：22 ms, 在所有 Java 提交中击败了6.30%的用户
+     * 内存消耗：47 MB, 在所有 Java 提交中击败了87.91%的用户
+     */
+}
