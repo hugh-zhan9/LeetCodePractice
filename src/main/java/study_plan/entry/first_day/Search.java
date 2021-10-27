40758fcf80e03ab4674948be2b2b934e93f14a40
晚青 <49809663+hugh-zhan9@users.noreply.github.com>
2021-10-27 12:35:00 +0800

    Create Search.java

---
+++ src/main/java/study_plan/entry/first_day/Search.java
@@ -0,0 +1,14 @@
+package study_plan.entry.first_day;
+
+/**
+ * @author zyk
+ * @description
+ * @since 2021/10/27 11:50
+ */
+public class Search {
+
+    public int search(int[] nums, int target) {
+        return -1;
+    }
+
+}
