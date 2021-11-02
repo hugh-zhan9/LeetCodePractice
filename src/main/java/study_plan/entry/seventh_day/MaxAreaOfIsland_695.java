442d434bb8cc8eef1ae6edd38723a4e48f7dc656
晚青 <49809663+hugh-zhan9@users.noreply.github.com>
2021-11-02 10:30:05 +0800

    2021-11-02 study_plan 695_MaxAreaOfIsland

--- src/main/java/study_plan/entry/seventh_day/MaxAreaOfIsland_695.java
+++ src/main/java/study_plan/entry/seventh_day/MaxAreaOfIsland_695.java
@@ -1,8 +1,8 @@
 package study_plan.entry.seventh_day;
 
 /**
- * 给你一个大小为 m x n 的二进制矩阵 grid 。
- * 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在 水平或者竖直的四个方向上 相邻。你可以假设 grid 的四个边缘都被 0（代表水）包围着。
+ * 给你一个大小为 mxn 的二进制矩阵 grid。
+ * 岛屿是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在水平或者竖直的四个方向上相邻。你可以假设 grid 的四个边缘都被 0（代表水）包围着。
  * 岛屿的面积是岛上值为 1 的单元格的数目。
  * 计算并返回 grid 中最大的岛屿面积。如果没有岛屿，则返回面积为 0 。
  *
@@ -38,7 +38,33 @@ package study_plan.entry.seventh_day;
 public class MaxAreaOfIsland_695 {
 
     public int maxAreaOfIsland(int[][] grid) {
-        return -1;
+        int res =0;
+        for (int i=0; i<grid.length; i++){
+            for (int j=0; j<grid[0].length; j++){
+                res = Math.max(res, dfs(i,j,grid));
+            }
+        }
+        return res;
     }
 
+
+    public int dfs (int i, int j, int[][] grid){
+        if (i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j] != 1){
+            return 0;
+        }else {
+            grid[i][j] = 2;
+            int ans = 1;
+            ans += dfs(i - 1, j, grid);
+            ans += dfs(i + 1, j, grid);
+            ans += dfs(i, j - 1, grid);
+            ans += dfs(i, j + 1, grid);
+            return ans;
+        }
+    }
+
+    /**
+     * 执行用时：2 ms, 在所有 Java 提交中击败了99.99%的用户
+     * 内存消耗：38.9 MB, 在所有 Java 提交中击败了51.84%的用户
+     */
+    
 }
