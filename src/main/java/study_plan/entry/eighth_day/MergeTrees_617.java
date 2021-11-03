41e5d035f9ad1180adce5412d9768a991d1f19c9
晚青 <49809663+hugh-zhan9@users.noreply.github.com>
2021-11-03 10:45:51 +0800

    2021-11-03 study_plan 617_MergeTrees

---
+++ src/main/java/study_plan/entry/eighth_day/MergeTrees_617.java
@@ -0,0 +1,154 @@
+package study_plan.entry.eighth_day;
+
+/**
+ * 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+ * 你需要将他们合并为一个新的二叉树。
+ * 合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+ *
+ * 示例 1:
+ * 输入:
+ * 	Tree 1                     Tree 2
+ *           1                         2
+ *          / \                       / \
+ *         3   2                     1   3
+ *        /                           \   \
+ *       5                             4   7
+ * 输出:
+ * 合并后的树:
+ * 	     3
+ * 	    / \
+ * 	   4   5
+ * 	  / \   \
+ * 	 5   4   7
+ * 注意: 合并必须从两个树的根节点开始。
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-binary-trees
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author zyk
+ * @description
+ * @since 2021/11/3 9:10
+ */
+public class MergeTrees_617 {
+
+
+    /** 深度优先算法 */
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        return dfs(root1,root2);
+    }
+
+    public TreeNode dfs(TreeNode root1, TreeNode root2){
+        TreeNode root = new TreeNode();
+        if (root1 != null && root2 != null){
+            root.val = root1.val + root2.val;
+            root.left = dfs(root1.left, root2.left);
+            root.right = dfs(root1.right, root2.right);
+        }else if (root1 != null){
+            root.val = root1.val;
+            root.left = root1.left;
+            root.right = root1.right;
+        }else if (root2 != null){
+            root.val = root2.val;
+            root.left = root2.left;
+            root.right = root2.right;
+        }else {
+            return null;
+        }
+        return root;
+    }
+
+    /**
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：38.4 MB, 在所有 Java 提交中击败了83.49%的用户
+     */
+
+
+    /** 👆你看这人像不像傻逼 */
+
+
+    /** 上面的写法优化后👇 */
+    public TreeNode dfsEnhance(TreeNode root1, TreeNode root2){
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+
+        /** 这样有一个问题就是，新的树中有部分是老树中的节点。最好还是替换成新的节点 */
+
+        TreeNode root = new TreeNode(root1.val + root2.val);
+        root.left = mergeTrees(root1.left, root2.left);
+        root.right = mergeTrees(root1.right, root2.right);
+        return root;
+    }
+
+
+
+    /** 广度优先算法 */
+    public TreeNode mergeTrees2(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        TreeNode merged = new TreeNode(t1.val + t2.val);
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        Queue<TreeNode> queue1 = new LinkedList<TreeNode>();
+        Queue<TreeNode> queue2 = new LinkedList<TreeNode>();
+        queue.offer(merged);
+        queue1.offer(t1);
+        queue2.offer(t2);
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            TreeNode node = queue.poll(), node1 = queue1.poll(), node2 = queue2.poll();
+            TreeNode left1 = node1.left, left2 = node2.left, right1 = node1.right, right2 = node2.right;
+            if (left1 != null || left2 != null) {
+                if (left1 != null && left2 != null) {
+                    TreeNode left = new TreeNode(left1.val + left2.val);
+                    node.left = left;
+                    queue.offer(left);
+                    queue1.offer(left1);
+                    queue2.offer(left2);
+                } else if (left1 != null) {
+                    node.left = left1;
+                } else if (left2 != null) {
+                    node.left = left2;
+                }
+            }
+            if (right1 != null || right2 != null) {
+                if (right1 != null && right2 != null) {
+                    TreeNode right = new TreeNode(right1.val + right2.val);
+                    node.right = right;
+                    queue.offer(right);
+                    queue1.offer(right1);
+                    queue2.offer(right2);
+                } else if (right1 != null) {
+                    node.right = right1;
+                } else {
+                    node.right = right2;
+                }
+            }
+        }
+        return merged;
+    }
+
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
