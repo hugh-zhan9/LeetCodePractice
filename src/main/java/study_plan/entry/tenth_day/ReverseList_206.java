ab8e279097d23ea3ac42b3c37027da21a0b70a5e
hugh-zhan9 <hugh_zhan9@163.com>
2021-11-05 07:36:08 +0800

    2021-11-05 study_plan 206_ReverseList

---
+++ src/main/java/study_plan/entry/tenth_day/ReverseList_206.java
@@ -0,0 +1,43 @@
+package study_plan.entry.tenth_day;
+
+/**
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ *
+ *
+ * 示例 1：
+ * ![img](https://assets.leetcode.com/uploads/2021/02/19/rev1ex1.jpg)
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ *
+ * 示例 2：
+ * ![img](https://assets.leetcode.com/uploads/2021/02/19/rev1ex2.jpg)
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ *
+ * 示例 3：
+ * 输入：head = []
+ * 输出：[]
+ *
+ * 提示：
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 <= Node.val <= 5000
+ *
+ * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-linked-list/
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * @author zyk
+ * @description
+ * @since 2021/11/5 7:34
+ */
+public class ReverseList_206 {
+
+    public ListNode reverseList(ListNode head) {
+        return null;
+    }
+
+}
