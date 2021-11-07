b7d140903584a94a6c9ccca815e6b620475b569d
hugh-zhan9 <hugh_zhan9@163.com>
2021-11-07 10:41:30 +0800

    2021-11-06 study_plan Permute_46

---
+++ src/main/java/study_plan/entry/twelve_day/Permute_46.java
@@ -0,0 +1,76 @@
+package study_plan.entry.twelve_day;
+
+/**
+ * 给定一个不含重复数字的数组 nums ，返回其所有可能的全排列 。你可以按任意顺序返回答案。
+ *
+ *
+ * 示例 1：
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ *
+ * 示例 2：
+ * 输入：nums = [0,1]
+ * 输出：[[0,1],[1,0]]
+ *
+ * 示例 3：
+ * 输入：nums = [1]
+ * 输出：[[1]]
+ *  
+ *
+ * 提示：
+ * 1 <= nums.length <= 6
+ * -10 <= nums[i] <= 10
+ * nums 中的所有整数 互不相同
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/permutations
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author zyk
+ * @description
+ * @since 2021/11/7 10:35
+ */
+public class Permute_46 {
+
+    /** 官方题解：回溯 */
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+
+        for (int num : nums) {
+            output.add(num);
+        }
+
+        int n = nums.length;
+        backtrack(n, output, res, 0);
+        return res;
+    }
+
+    public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+        // 所有数都填完了
+        if (first == n) {
+            res.add(new ArrayList<Integer>(output));
+        }
+        for (int i = first; i < n; i++) {
+            // 动态维护数组
+            Collections.swap(output, first, i);
+            // 继续递归填下一个数
+            backtrack(n, output, res, first + 1);
+            // 撤销操作
+            Collections.swap(output, first, i);
+        }
+    }
+
+
+    /**
+     * Java基础打的还不牢固，res.add(path), 这个地方明明只是引用，
+     * 我们需要的是其中的在某个特定时期的内容，所以一定要进行拷贝。抄了好几遍代码都不懂，结果在这两句话就完事了。谢谢威哥。
+     */
+
+}
