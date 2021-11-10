40b2f1a294939c123e446acad821240803f3ac12
hugh-zhan9 <hugh_zhan9@163.com>
2021-11-10 20:55:00 +0800

    2021-11-07 study_plan 70_ClimbStairs

--- src/main/java/not_do/Rob_198_not_do.java
+++ src/main/java/not_do/Rob_198_not_do.java
@@ -55,4 +55,19 @@ public class Rob_198_not_do {
     }
 
 
+    public int rob_dp(int[] nums) {
+        int[] dp = new int[nums.length+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i=2; i<=nums.length; i++){
+            dp[i] = Math.max(dp[i-2]+nums[i-1], dp[i-1]);
+        }
+        return dp[nums.length];
+    }
+
+    /**
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：35.7 MB, 在所有 Java 提交中击败了64.99%的用户
+     */
+
 }
